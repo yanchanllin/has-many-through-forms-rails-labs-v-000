46819454f39de8a0631f72aa6a7ce7693b37eeb1
YanC Lin <yonalin1@gmail.com>
2018-04-21 04:58:25 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank?}
- 
+
    def user_attributes(user_attributes)
      user_attributes.values.each do |user_attributes|
        if user_attributes[:username].present?
